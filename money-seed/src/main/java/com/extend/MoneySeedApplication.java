306e13faf8a38028a983f5282f6b5493d229cab5
yonghw <thirdhacker@naver.com>
2020-06-26 23:48:20 +0900

    init

---
+++ money-seed/src/main/java/com/extend/MoneySeedApplication.java
@@ -0,0 +1,13 @@
+package com.extend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneySeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneySeedApplication.class, args);
+	}
+
+}
---
+++ money-seed/src/test/java/com/extend/MoneySeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneySeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
